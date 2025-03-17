b5186ff12559d330b828095b20e6878b3a242d07
Xandra Dave Cochran <Xan.Cochran@ed.ac.uk>
2025-03-17 23:30:29 +0000

    Gimme SLURM lovin

--- run_models.sh
+++ run_models.sh
@@ -94,18 +94,18 @@ mkdir -p ${SCRATCH_HOME}
 echo "echo echo echo echo echo"
 
 # Create and activate your conda environment
-ENV_NAME=philos_env
-# create venv, ~20 minutes
-echo "make environment with python 3.10"
-/opt/conda/bin/python3.10 -m venv "${ENV_NAME}"
-echo "activate env"
+# ENV_NAME=philos_env
+# # create venv, ~20 minutes
+# echo "make environment with python 3.10"
+# /opt/conda/bin/python3.10 -m venv "${ENV_NAME}"
+# echo "activate env"
 source "${ENV_NAME}/bin/activate"
-which python
-echo "upgrading pip"
-# ~20 minutes
-pip install --upgrade pip
-echo "install packages"
-pip install -r requirements.txt
+# which python
+# echo "upgrading pip"
+# # ~20 minutes
+# pip install --upgrade pip
+# echo "install packages"
+# pip install -r requirements.txt
 
 
 
