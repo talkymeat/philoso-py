09e6a49bc33816fbeacd3cbf7fe7c10d43e98d5e
Xandra Dave Cochran <Xan.Cochran@ed.ac.uk>
2025-03-22 20:43:08 +0000

    correction to test

--- test_devices.py
+++ test_devices.py
@@ -5,11 +5,11 @@ model = ModelFactory().from_json('model_json/model1_gpu.json')
 for agent in model.agents:
     for name, net in model.agents[0].nn.policy_layers.items():
         for param in net.parameters():
-            assert param.device[:4] == 'cuda'
+            assert str(param.device)[:4] == 'cuda'
 
     for name, net in model.agents[0].nn.policy_heads.items():
         for nom, head in net.items():
             for param in head.parameters():
-                assert param.device[:4] == 'cuda'
+                assert str(param.device)[:4] == 'cuda'
 
 print('devices test OK')
