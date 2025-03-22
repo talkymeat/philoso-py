b518e4d512509aece4cb37bbbb751e9c20556da5
Xandra Dave Cochran <Xan.Cochran@ed.ac.uk>
2025-03-22 20:39:57 +0000

    Test that all networks in agents set device correctly

---
+++ test_devices.py
@@ -0,0 +1,13 @@
+from philoso_py import ModelFactory
+
+model = ModelFactory().from_json('model_json/model1_gpu.json')
+
+for agent in model.agents:
+    for name, net in model.agents[0].nn.policy_layers.items():
+        for param in net.parameters():
+            assert param.device[:4] == 'cuda'
+
+    for name, net in model.agents[0].nn.policy_heads.items():
+        for nom, head in net.items():
+            for param in head.parameters():
+                assert param.device[:4] == 'cuda'
