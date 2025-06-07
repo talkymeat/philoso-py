244d9e5565f6a97f085d105eeef5cbe31316ab71
Xandra Dave Cochran <Xan.Cochran@ed.ac.uk>
2025-06-07 11:16:28 +0100

    More detailed progress check for Eddie

--- prog_check.py
+++ prog_check.py
@@ -1,6 +1,6 @@
-import os
-from glob import glob
 from pathlib import Path
+import re
+from datetime import datetime
 
 def count_days(path: str):
     return len(glob(str(Path(path) / 'a_0' / 'days')+os.sep+'*'))//4
@@ -16,9 +16,42 @@ def show(report_: dict):
     for k, v in report_.items():
         print(f'{k: <{maxlen}} | {"#"*v:-<{100}} {v}')
 
+def ag_folders(f: str):
+    return [f_+os.sep+'gp_out' for f_ in folders(f) if f_.split(os.sep)[-1].startswith('a_')]
+
+def list_flatten(ls: list[list]):
+    new_ls = []
+    for it in ls:
+        if isinstance(it, list):
+            new_ls += list_flatten(it)
+        else: 
+            new_ls.append(it)
+    return new_ls
+
+def max_t(fname: str) -> int:
+    ls = list_flatten([folders(f) for f in ag_folders(fname)])
+    ls = [f_.split(os.sep)[-1].split('_')[-1] for f_ in list_flatten([folders(f) for f in ls])]
+    ls = [t for t in ls if re.match(r't[0-9]+', t)]
+    vals = [1+int(t[1:]) for t in ls]
+    return max(vals) if vals else 0
+
+def detailed_report(root: str):
+    return {f.split(os.sep)[-1]: max_t(f) for f in folders(root)}
+
+def show_detailed(report_: dict):
+    maxlen = max([len(k) for k in report_.keys()])
+    print(datetime.now())
+    for k, v in report_.items():
+        try:
+            print(f'{k: <{maxlen}} | {"#"*(v//100):-<{100}} <hundreds>\n{" "*maxlen} | {"="*(v%100):-<{100}} <units> total: {v}')
+        except TypeError as e:
+            print(k, v, type(v))
+            raise e
+
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser(prog='prog_check')
     parser.add_argument('root', default='')
     args = parser.parse_args()
-    show(report(str(Path(args.root)/'philoso-py'/'output')))
+    show_detailed(detailed_report(str(Path(args.root)/'philoso-py'/'output')))
